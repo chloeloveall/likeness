84a9fd15db00396ef86b82d59d5582d73402d2f9
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-07-24 13:53:15 -0700

    Create photo upload progress bar with animation and useEffect hook to remove bar and file name after upload

---
+++ src/components/Photos/ProgressBar.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import useFirebaseStorage from '../../hooks/useFirebaseStorage';
+import { motion } from 'framer-motion';
+import { StyledProgressBar } from './styles';
+
+export default function ProgressBar({ file, setFile }) {
+  const { url, progress } = useFirebaseStorage(file);
+  
+  useEffect(() => {
+    if (url) {
+      setFile(null);
+    }
+  }, [url, setFile]);
+
+  return (
+    <StyledProgressBar
+      as={motion.div}
+      initial={{ width: 0 }}
+      animate={{ width: progress + '%' }}
+    />
+  );
+};

82f8319405adcc3154d0d2910e5ac7dbe940b4f2
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-07-23 23:41:04 -0700

    Remove unused container styled component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,20 +8,21 @@ export default function Navbar() {
   return (
     <>
       <GlobalStyle />
-      <Nav>
-        <div className="logo">
-          <Heading1 as={Link} to='/home'>
-            <Likeness 
-              as={motion.span} 
-              whileHover={{ backgroundImage: 'linear-gradient(to right, #D6A4A4 0%, #DAE2F8  51%, #D6A4A4  100%)', backgroundPosition: 'right center' }}
-            >
-              likeness
-            </Likeness>
-          </Heading1>
-        </div>
-        <Burger />
-      </Nav>
+      <Container>
+        <Nav>
+          <div className="logo">
+            <Heading1 as={Link} to='/home'>
+              <Likeness 
+                as={motion.span} 
+                whileHover={{ backgroundImage: 'linear-gradient(to right, #D6A4A4 0%, #DAE2F8  51%, #D6A4A4  100%)', backgroundPosition: 'right center' }}
+              >
+                likeness
+              </Likeness>
+            </Heading1>
+          </div>
+          <Burger />
+        </Nav>
+      </Container>
     </>
   );
 };
-

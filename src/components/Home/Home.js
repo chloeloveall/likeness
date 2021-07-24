60d686243a79d4a16df2bc11d4eb29cb585b8fd9
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-07-24 14:22:05 -0700

    Disable background scroll when photo modal is open

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,14 +2,17 @@ import React, { useState } from 'react';
 import PhotoUploadForm from '../Photos/PhotoUploadForm';
 import PhotoGrid from '../Photos/PhotoGrid';
 import PhotoModal from '../Photos/PhotoModal';
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
 export default function Home() {
   const [selectedImage, setSelectedImage] = useState(null);
 
+  selectedImage !== null ? disableBodyScroll(document.body) : enableBodyScroll(document.body);
+
   return (
     <>
       <PhotoUploadForm />
-      <PhotoGrid setSelectedImage={setSelectedImage} />
+      <PhotoGrid setSelectedImage={setSelectedImage}  />
       {selectedImage &&
         <PhotoModal selectedImage={selectedImage} setSelectedImage={setSelectedImage} />}
     </>

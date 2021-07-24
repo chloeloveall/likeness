522f43f431b2e82eee0ae674b251f72598884617
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-07-23 23:47:33 -0700

    Remove unused container styled component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { GlobalStyle, Heading1, Likeness, Container, Nav } from './styles';
+import { GlobalStyle, Heading1, Likeness, Nav } from './styles';
 import { motion } from 'framer-motion';
 import Burger from './Burger';
 
@@ -8,21 +8,19 @@ export default function Navbar() {
   return (
     <>
       <GlobalStyle />
-      <Container>
-        <Nav>
-          <div className="logo">
-            <Heading1 as={Link} to='/home'>
-              <Likeness 
-                as={motion.span} 
-                whileHover={{ backgroundImage: 'linear-gradient(to right, #D6A4A4 0%, #DAE2F8  51%, #D6A4A4  100%)', backgroundPosition: 'right center' }}
-              >
-                likeness
-              </Likeness>
-            </Heading1>
-          </div>
-          <Burger />
-        </Nav>
-      </Container>
+      <Nav>
+        <div className="logo">
+          <Heading1 as={Link} to='/home'>
+            <Likeness 
+              as={motion.span} 
+              whileHover={{ backgroundImage: 'linear-gradient(to right, #D6A4A4 0%, #DAE2F8  51%, #D6A4A4  100%)', backgroundPosition: 'right center' }}
+            >
+              likeness
+            </Likeness>
+          </Heading1>
+        </div>
+        <Burger />
+      </Nav>
     </>
   );
 };

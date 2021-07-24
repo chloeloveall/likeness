fba3d4e1a65354581615ab19cc445cc872b309f4
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-07-23 18:36:40 -0700

    Create nav bar styled components for container,logo, nav, and global styles

---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body{
+    height: 100vh;
+    width: 80vw;
+    font-family: 'Monda', sans-serif;
+    background-image: linear-gradient(to top, #f3e7e9 0%, #e3eeff 99%, #e3eeff 100%);
+    background-attachment: fixed;
+    bottom: 0;
+    margin: 0 auto;
+  }
+`;
+
+export const Container = styled.div`
+  margin: auto;
+  width: 80vw;
+  margin-top: 20px;
+`;
+
+export const Heading1 = styled.h1`
+  font-size: 40px;
+  font-family: 'Julius Sans One', sans-serif;
+  text-decoration: none;
+  font-weight: bold;
+`;
+
+export const Likeness = styled.span`
+  background-image: linear-gradient(to right, #D6A4A4 0%, #D6A4A4  51%, #D6A4A4  100%);
+  background-repeat: repeat;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -webkit-font-smoothing: antialiased;
+  transition: 0.5s;
+  background-size: 200% auto;
+`;
+
+export const Nav = styled.nav`
+  width: 100%;
+  height: 55px;
+  display: flex;
+  justify-content: space-between;
+  .logo {
+    padding-top: 20px;
+  }
+`;

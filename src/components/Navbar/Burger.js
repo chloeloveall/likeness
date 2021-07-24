afb2ad6a634394bba4540576e2f08442f9f6538f
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-07-23 23:40:19 -0700

    Add burger menu component, use state to set sidebar open, prevent scroll with bodyscrolllock

---
+++ src/components/Navbar/Burger.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+import { StyledBurger } from './styles';
+import RightNav from './RightNav';
+import { motion } from 'framer-motion';
+
+export default function Burger() {
+  const [open, setOpen] = useState(false);
+  open ? disableBodyScroll(document.body) : enableBodyScroll(document.body);
+
+  return (
+    <>
+      {open && 
+        <motion.div 
+          className='backdrop' 
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+        />}
+      <StyledBurger className='scrollSettings' open={open} onClick={() => setOpen(!open)}>
+        <div className='burgerSection' />
+        <div className='burgerSection'/>
+        <div className='burgerSection'/>
+      </StyledBurger>
+      <RightNav open={open} setOpen={setOpen}/>
+    </>
+  );
+};

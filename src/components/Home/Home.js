9d1a41f2ca841e7dfcc7fc98132b686666218a9d
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-07-24 13:48:24 -0700

    Update file path names

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import UploadForm from '../PhotoUploadForm';
-import PhotoGrid from '../PhotoGrid';
-import PhotoModal from '../PhotoModal';
+import PhotoUploadForm from '../Photos/PhotoUploadForm';
+import PhotoGrid from '../Photos/PhotoGrid';
+import PhotoModal from '../Photos/PhotoModal';
 
 export default function Home() {
   const [selectedImage, setSelectedImage] = useState(null);
 
   return (
     <>
-      <UploadForm />
+      <PhotoUploadForm />
       <PhotoGrid setSelectedImage={setSelectedImage} />
       {selectedImage &&
         <PhotoModal selectedImage={selectedImage} setSelectedImage={setSelectedImage} />}

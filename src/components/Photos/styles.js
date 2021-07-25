65c9e7bcd81a59b934db15f1c50a951d8954846f
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-07-24 22:46:30 -0700

    Remove test styles

--- src/components/Photos/styles.js
+++ src/components/Photos/styles.js
@@ -17,7 +17,6 @@ export const PhotoGridStyle = styled.div`
 export const ImageWrap = styled.div`
   overflow: hidden;
   cursor: pointer;
-	overflow: hidden;
   height: 0;
   ${'' /* padding to create square image regardless of width */}
   padding: 50% 0;
@@ -39,8 +38,49 @@ export const ImageWrap = styled.div`
 `;
 
 // photo modal styles
+export const StyledPhotoModal = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.5);
+  z-index: 30;
+  img {
+    display: block;
+    max-width: 80vw;
+    max-height: 80vh;
+    box-shadow: 3px 5px 7px rgba(0,0,0,0.5);
+    border: 1px solid white;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
 
-// photo upload styles
+export const StickyControls = styled.div`
+  min-height: 100vh;
+  min-width: 100vw;
+  display: grid;
+  grid-template-rows: auto 1fr 9.5vh;
+  margin: auto;
+  top: 0;
+  position: fixed;
+  cursor: pointer;
+`;
+
+export const Button = styled.button`
+  display: block;
+  background: #F2F4F8;
+  border: none;
+  margin: 5px;
+`;
+
+// photo upload form styles
+// export const StyledProgressBar = styled.div`
+  
+// `;
 
 // upload progress bar styles 
 export const StyledProgressBar = styled.div`

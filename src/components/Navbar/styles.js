3a338a4c533408ae6294f7fcd8899b23274ac739
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-07-24 20:15:08 -0700

    Fix typo

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -110,14 +110,14 @@ export const Ul = styled.ul`
   } */}
   .gradient {
     background-image:#DAE2f8;background-image:  
-    radial-gradient(at 24% 25%, hsla(224,68%,89%,1) 0, 255, 255, 255, 0 51%),  
-    radial-gradient(at 44% 35%, hsla(0,38%,68%,1) 0, 255, 255, 255, 0 46%),  
-    radial-gradient(at 66% 70%, hsla(224,69%,92%,1) 0, 255, 255, 255, 0 45%),  
-    radial-gradient(at 91% 70%, hsla(0,38%,75%,1) 0, 255, 255, 255, 0 45%),  
-    radial-gradient(at 93% 10%, hsla(217,100%,95%,1) 0, 255, 255, 255, 0 31%),  
-    radial-gradient(at 44% 81%, hsla(355,85%,93%,1) 0, 255, 255, 255, 0 50%),  
-    radial-gradient(at 80% 100%, hsla(224,69%,92%,1) 0, 255, 255, 255, 0 50%),  
-    radial-gradient(at 0% 0%, hsla(351,34%,93%,1) 0, 255, 255, 255, 0 50%);
+    radial-gradient(at 24% 25%, hsla(224,68%,89%,1) 0, rgba(255, 255, 255, 0) 51%),  
+    radial-gradient(at 44% 35%, hsla(0,38%,68%,1) 0, rgba(255, 255, 255, 0) 46%),  
+    radial-gradient(at 66% 70%, hsla(224,69%,92%,1) 0, rgba(255, 255, 255, 0) 45%),  
+    radial-gradient(at 91% 70%, hsla(0,38%,75%,1) 0, rgba(255, 255, 255, 0) 45%),  
+    radial-gradient(at 93% 10%, hsla(217,100%,95%,1) 0, rgba(255, 255, 255, 0) 31%),  
+    radial-gradient(at 44% 81%, hsla(355,85%,93%,1) 0, rgba(255, 255, 255, 0) 50%),  
+    radial-gradient(at 80% 100%, hsla(224,69%,92%,1) 0, rgba(255, 255, 255, 0) 50%),  
+    radial-gradient(at 0% 0%, hsla(351,34%,93%,1) 0, rgba(255, 255, 255, 0) 50%);
     height: 60vh;
     width: 60vh;
     margin: auto;
@@ -145,6 +145,7 @@ export const StyledLink = styled(Link)`
   font-size: 50px;
   text-decoration: none;
   color: #4e4e4e;
+  ${'' /* font-family: 'Monda', sans-serif; */}
   &:hover {
     background-image: linear-gradient(to right, #D6A4A4 0%, #D6A4A4  51%, #D6A4A4  100%);
     background-repeat: repeat;

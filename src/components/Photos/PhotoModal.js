95ccdc06a45bfd3755f16140fe7908c82e3669af
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-07-24 22:47:46 -0700

    Add photo view modal

---
+++ src/components/Photos/PhotoModal.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { StyledPhotoModal, Button } from './styles';
+import { XCircle } from "phosphor-react";
+
+export default function Modal({ selectedImage, setSelectedImage }) {
+  function handleClose(event) {
+    if (event.target.classList.contains('closeButton')) {
+      setSelectedImage(null);
+    };
+  };
+
+  return (
+    <StyledPhotoModal
+      as={motion.div}
+      className='backdrop' 
+      onClick={handleClose}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
+      {/* <motion.img 
+        src={selectedImage} 
+        alt='Uncropped image' 
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+      /> */}
+
+      <motion.img 
+        src={selectedImage} 
+        alt='Uncropped image' 
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+      />
+      
+      <Button className='closeButton'>
+        <XCircle size={48} className='closeButton'/>
+      </Button>
+      {/* <Button className='deleteButton'>
+        <XCircle size={48} className='deleteButton'/>
+      </Button> */}
+    </StyledPhotoModal>
+  );
+};

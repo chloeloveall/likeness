fbc231d095ea418b8f98416a63e9b9226ee32236
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-07-24 20:02:55 -0700

    Temp removal of unused routes

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -3,9 +3,11 @@ import { Route, useLocation } from 'react-router-dom';
 
 import Landing from '../Landing/Landing';
 import Home from '../Home/Home';
-import LoginForm from '../Auth/LoginForm';
+// import LoginForm from '../Auth/LoginForm';
 import Navbar from '../Navbar/Navbar';
 import { Container } from './styles';
+// import About from '../About/About';
+// import Contact from '../Contact/Contact';
 
 export default function Routes() {
   const location = useLocation();
@@ -16,8 +18,10 @@ export default function Routes() {
         <Navbar />}
       <Route exact path='/' component={Landing} />
       <Container>
-        <Route path='/login' component={LoginForm} />
+        {/* <Route path='/login' component={LoginForm} /> */}
         <Route path='/home' component={Home} />
+        {/* <Route path='/about' component={About} /> */}
+        {/* <Route path='/contact' component={Contact} /> */}
       </Container>
     </>
   );
